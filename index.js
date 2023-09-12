287ce2b2cfa3d29ed7aae963a41f8645b64867e0
Matthias Kleine <github.com9@emailu.de>
2023-09-12 09:35:09 +0200

    New-Issue

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
 const axios = require('axios').default;
 const generateReadmeMarkdown = require(__dirname + '/lib/readme').generateMarkdown;
+const generateNewIssueTemplate = require(__dirname + '/lib/issue-template').generateNewIssueTemplate;
 
 const user = 'iobroker-community-adapters';
-const packageName = 'shelly';
+const packageName = 'proxmox';
 
 axios
     .get(`https://api.github.com/repos/${user}/iobroker.${packageName}`)
     .then(response => {
         if (response.status === 200) {
             console.log(generateReadmeMarkdown(user, packageName, response.data));
+            console.log(generateNewIssueTemplate(user, packageName, response.data));
         } else {
             console.error('GitHub request failed')
         }
