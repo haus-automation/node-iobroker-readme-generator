03f69333591cb914ebb0d857c97ce643cf076504
Matthias Kleine <github.com9@emailu.de>
2023-12-26 19:04:40 +0100

    generateNewIssueWorkflow

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Helper script to generate shield.io readme for ioBroker adapters
 
 The MIT License (MIT)
 
-Copyright (c) 2022 Matthias Kleine <info@haus-automatisierung.com>
+Copyright (c) 2024 Matthias Kleine <info@haus-automatisierung.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const axios = require('axios').default;
 const generateReadmeMarkdown = require(__dirname + '/lib/readme').generateMarkdown;
-const generateNewIssueTemplate = require(__dirname + '/lib/issue-template').generateNewIssueTemplate;
+const generateNewIssueWorkflow = require(__dirname + '/lib/issue-template').generateNewIssueWorkflow;
 
-const user = 'iobroker-community-adapters';
-const packageName = 'proxmox';
+const user = 'iobroker';
+const packageName = 'node-red';
 
 axios
     .get(`https://api.github.com/repos/${user}/iobroker.${packageName}`)
     .then(response => {
         if (response.status === 200) {
             console.log(generateReadmeMarkdown(user, packageName, response.data));
-            console.log(generateNewIssueTemplate(user, packageName, response.data));
+            console.log(generateNewIssueWorkflow(user, packageName, response.data));
         } else {
             console.error('GitHub request failed')
         }
